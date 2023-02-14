0bf95e44ae501a5eab639d057a601fd30849be0e
Francisco Yago <90482427+franyagodev@users.noreply.github.com>
2023-02-15 00:42:28 +0100

    switch to english

--- enums.java
+++ enums.java
@@ -1,42 +1,42 @@
-public enum TipoLicencia {
+ public enum LicenseType {
     FREE(0, new String[][]{{"en", "Free license"}, {"es", "Licencia gratuita"}}, 0.0),
     FREEMIUM(1, new String[][]{{"en", "Freemium license"}, {"es", "Licencia freemium"}}, 5.99),
     PREMIUM(2, new String[][]{{"en", "Premium license"}, {"es", "Licencia premium"}}, 9.99),
     GOLD(3, new String[][]{{"en", "Gold license"}, {"es", "Licencia gold"}}, 19.99);
 
     private final int id;
-    private final String[][] descripciones;
-    private double precio;
+    private final String[][] descriptions;
+    private double price;
 
-    private TipoLicencia(int id, String[][] descripciones, double precio) {
+    private LicenseType(int id, String[][] descriptions, double price) {
         this.id = id;
-        this.descripciones = descripciones;
-        this.precio = precio;
+        this.descriptions = descriptions;
+        this.price = price;
     }
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public void setDescripcion(String idioma, String descripcion) {
-        for (String[] descrip : descripciones) {
-            if (descrip[0].equals(idioma)) {
-                descrip[1] = descripcion;
-                break;
-            }
-        }
-    }
-
-    public String getDescripcion(String idioma) {
-        for (String[] descrip : descripciones) {
-            if (descrip[0].equals(idioma)) {
-                return descrip[1];
+    public String getDescription(String languageCode) {
+        for (String[] description : descriptions) {
+            if (description[0].equals(languageCode)) {
+                return description[1];
             }
         }
         return "";
     }
 
-    public double getPrecio() {
-        return this.precio;
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String languageCode, String description) {
+        for (String[] descriptionArray : descriptions) {
+            if (descriptionArray[0].equals(languageCode)) {
+                descriptionArray[1] = description;
+                break;
+            }
+        }
     }
 }
